ff2599a83fb353ee43d9f215dbdd9c2e36eb984b
CuongTu1806 <nguyenbacuong18042004@gmail.com>
2026-03-10 18:45:59 +0700

    initial spring boot project

---
+++ src/test/java/com/example/learningVocabularyPlatform/LearningVocabularyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningVocabularyPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningVocabularyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
