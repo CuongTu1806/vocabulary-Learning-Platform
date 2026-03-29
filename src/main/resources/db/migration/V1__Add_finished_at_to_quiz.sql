650738f1c821b91491e360fd048a2d60db76c24c
CuongTu1806 <nguyenbacuong18042004@gmail.com>
2026-03-29 19:29:40 +0700

    completed

--- src/main/resources/db/migration/V1__Add_finished_at_to_quiz.sql
+++
@@ -1,6 +0,0 @@
--- Add finished_at column to quiz table
-ALTER TABLE quiz ADD COLUMN finished_at DATETIME NULL;
-
--- Optional: Add index for better filtering/sorting performance
--- CREATE INDEX idx_quiz_duration ON quiz(duration);
--- CREATE INDEX idx_quiz_finished_at ON quiz(finished_at);
