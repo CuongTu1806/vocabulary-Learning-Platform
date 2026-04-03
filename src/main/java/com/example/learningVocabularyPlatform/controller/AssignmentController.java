239c4b82ae8f38d5c790f89ce4c316dd437658e8
Su <hoanganhng293@gmail.com>
2026-04-03 22:32:54 +0700

    initial assignment module

--- src/main/java/com/example/learningVocabularyPlatform/controller/AssignmentController.java
+++ src/main/java/com/example/learningVocabularyPlatform/controller/AssignmentController.java
@@ -1,4 +1,30 @@
 package com.example.learningVocabularyPlatform.controller;
+//	/api/assignments
+//Create assignment	POST /api/assignments
+//Update assignment	PUT /api/assignments/{id}
+//Delete assignment	DELETE /api/assignments/{id}
+//Get assignments	GET /api/assignments
+//Submit assignment	POST /api/assignments/{id}/submit
+//Get submission	GET /api/assignments/{id}/submissions
 
+
+import com.example.learningVocabularyPlatform.dto.response.ApiResponse;
+import com.example.learningVocabularyPlatform.service.AssignmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assignments")
+@RequiredArgsConstructor
 public class AssignmentController {
+
+    private final AssignmentService assignmentService;
+
+//    @PostMapping
+//    public ResponseEntity<ApiResponse> createAssignment() {
+//        return ResponseEntity.ok(assignmentService.createAssignment());
+//    }
 }
